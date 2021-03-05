8a88692e6ebf56fb784c912f5fd75e0766cdeb42
Max Courneya <max@mlcgaming.com>
2021-03-05 16:42:53 -0600

    Converted SQL scripts into programatic scripts to be run from inside the application.

--- Data/Management/DatabaseManager.cs
+++ Data/Management/DatabaseManager.cs
@@ -84,6 +84,96 @@ namespace RetroCollector.Data.Management {
             // Load all Transactions
             GetAllTransactions(dbConn);
         }
+        private static void InsertDefaultData() {
+            // Create Default Companies
+            string defaultCompanies =
+                $"INSERT INTO retrocollector.companies VALUES(0, 'RetroWave Collections', '2021-03-01 00:00:00', '2021-03-01 00:00:00', 'Built-In Admin', 'Built-In Admin');" +
+                $"INSERT INTO retrocollector.companies VALUES(1, 'Nintendo', '2021-03-01 00:00:00', '2021-03-01 00:00:00', 'Built-In Admin', 'Built-In Admin');" +
+                $"INSERT INTO retrocollector.companies VALUES(2, 'Sega', '2021-03-01 00:00:00', '2021-03-01 00:00:00', 'Built-In Admin', 'Built-In Admin');" +
+                $"INSERT INTO retrocollector.companies VALUES(3, 'Sony', '2021-03-01 00:00:00', '2021-03-01 00:00:00', 'Built-In Admin', 'Built-In Admin');" +
+                $"INSERT INTO retrocollector.companies VALUES(4, 'Insomniac Games', '2021-03-01 00:00:00', '2021-03-01 00:00:00', 'Built-In Admin', 'Built-In Admin');" +
+                $"INSERT INTO retrocollector.companies VALUES(5, 'Zener Works', '2021-03-01 00:00:00', '2021-03-01 00:00:00', 'Built-In Admin', 'Built-In Admin');";
+
+            string defaultProductTypes =
+                $"INSERT INTO retrocollector.producttypes VALUES(0, 'Video Game');" +
+                $"INSERT INTO retrocollector.producttypes VALUES(1, 'Console');" +
+                $"INSERT INTO retrocollector.producttypes VALUES(2, 'Merchandise');" +
+                $"INSERT INTO retrocollector.producttypes VALUES(3, 'Peripheral');" +
+                $"INSERT INTO retrocollector.producttypes VALUES(4, 'Service');";
+
+            string defaultConsoles =
+                $"INSERT INTO retrocollector.consoles VALUES(0, 'NONE', 'NONE');" +
+                $"INSERT INTO retrocollector.consoles VALUES(1, 'Nintendo 64', 'Nintendo');" +
+                $"INSERT INTO retrocollector.consoles VALUES(2, 'Sony Playstation (PSX)', 'Sony');" +
+                $"INSERT INTO retrocollector.consoles VALUES(3, 'Super Nintendo', 'Nintendo');" +
+                $"INSERT INTO retrocollector.consoles VALUES(4, 'Original Gameboy', 'Nintendo');" +
+                $"INSERT INTO retrocollector.consoles VALUES(5, 'Sega Genesis', 'Sega');" +
+                $"INSERT INTO retrocollector.consoles VALUES(6, 'Playstation 2', 'Sony');";
+
+            string defaultProducts =
+                $"INSERT INTO retrocollector.products VALUES(0, 'Super Nintendo Entertainment System [CIB]', 3, 1, '1991-8-23 00:00:00', 0, 2, 1, 1, 35.00, 45.99, '2021-03-01 00:00:00', '2021-03-01 00:00:00', 'Built-In Admin', 'Built-In Admin');" +
+                $"INSERT INTO retrocollector.products VALUES(1, 'Nintendo 64 System [Loose]', 1, 1, '1996-9-29 00:00:00', 0, 1, 1, 1, 45.00, 55.99, '2021-03-01 00:00:00', '2021-03-01 00:00:00', 'Built-In Admin', 'Built-In Admin');" +
+                $"INSERT INTO retrocollector.products VALUES(2, 'Super Mario World', 3, 1, '1991-8-23 00:00:00', 0, 1, 1, 0, 30.79, 49.99, '2021-03-01 00:00:00', '2021-03-01 00:00:00', 'Built-In Admin', 'Built-In Admin');" +
+                $"INSERT INTO retrocollector.products VALUES(3, 'Donkey Kong 64', 1, 1, '1999-11-4 00:00:00', 0, 1, 1, 0, 34.99, 44.95, '2021-03-01 00:00:00', '2021-03-01 00:00:00', 'Built-In Admin', 'Built-In Admin');" +
+                $"INSERT INTO retrocollector.products VALUES(4, 'Spyro the Dragon', 2, 4, '1998-9-9 00:00:00', 0, 1, 1, 0, 12.95, 17.99, '2021-03-01 00:00:00', '2021-03-01 00:00:00', 'Built-In Admin', 'Built-In Admin');" +
+                $"INSERT INTO retrocollector.products VALUES(5, 'Okage Shadow King', 6, 5, '2001-10-1 00:00:00', 0, 1, 1, 0, 19.69, 24.95, '2021-03-01 00:00:00', '2021-03-01 00:00:00', 'Built-In Admin', 'Built-In Admin');" +
+                $"INSERT INTO retrocollector.products VALUES(6, 'Mario Kart 64', 1, 1, '1997-2-10 00:00:00', 0, 1, 1, 0, 24.99, 39.99, '2021-03-01 00:00:00', '2021-03-01 00:00:00', 'Built-In Admin', 'Built-In Admin');" +
+                $"INSERT INTO retrocollector.products VALUES(7, 'Nintendo 64 Controller (Blue)', 1, 1, '1996-9-29 00:00:00', 0, 1, 1, 3, 15.99, 21.95, '2021-03-01 00:00:00', '2021-03-01 00:00:00', 'Built-In Admin', 'Built-In Admin');" +
+                $"INSERT INTO retrocollector.products VALUES(8, 'Pikachu Plush (1998)', NULL, 1, '1998-1-1 00:00:00', 0, 1, 1, 2, 23.95, 34.99, '2021-03-01 00:00:00', '2021-03-01 00:00:00', 'Built-In Admin', 'Built-In Admin');" +
+                $"INSERT INTO retrocollector.products VALUES(9, 'Console Repair Service', NULL, 0, '1998-1-1 00:00:00', 0, 1, 1, 4, 15.00, 75.00, '2021-03-01 00:00:00', '2021-03-01 00:00:00', 'Built-In Admin', 'Built-In Admin');";
+
+            string defaultUserRoles =
+                $"INSERT INTO retrocollector.userroles VALUES(0, 'Admin', 'Built-In Administrator Account', true, true, true, true, true, true, true, true, true, true, true, true, '2021-03-01 00:00:00', '2021-03-01 00:00:00', 'Built-In Admin', 'Built-In Admin');" +
+                $"INSERT INTO retrocollector.userroles VALUES(1, 'User', 'Built-In Standard User Account', true, true, false, false, false, false, false, false, false, false, true, false, '2021-03-01 00:00:00', '2021-03-01 00:00:00', 'Built-In Admin', 'Built-In Admin');";
+
+            string defaultUsers =
+                $"INSERT INTO retrocollector.users VALUES(0, 'Admin', '', 'Admin', 'Y3NhN68hLclf9oCLY6U89OFat3a+HsVYATg8WjxnydFM2w74m80x5mLfVbR9h8N0wpWECopUesKSyuG2oV+j9w==', 'TNsO+JvNMeZi31W0fYfDdMKVhAqKVHrCksrhtqFfo/c=', '2021-03-01 00:00:00', '2021-03-01 00:00:00', 'Built-In Admin', 'Built-In Admin', 0);";
+
+            string defaultCustomers =
+                $"INSERT INTO retrocollector.customers VALUES(0, 'Guest', 'Customer', '', '', '', '', '', '', '');" +
+                $"INSERT INTO retrocollector.customers VALUES(1, 'John', 'Smith', '123 N Broadway', 'Suite 103', 'Rochester', 'MN', '55906', '111-222-3333', 'jsmith@everyman.com');";
+
+            string defaultTransactions =
+                $"INSERT INTO retrocollector.transactions VALUES (0, 'Sample Sale', '2021-1-1 08:00:00', 0, 1, '2021-03-01 00:00:00', '2021-03-01 00:00:00', 'Built-In Admin', 'Built-In Admin');";
+
+            string defaultTransactionLineItems =
+                $"INSERT INTO retrocollector.transactionLineItems VALUES (0, 0, 1, 45.99);" +
+                $"INSERT INTO retrocollector.transactionLineItems VALUES(0, 1, 1, 60.00);";
+
+            string[] defaultCommands = new string[] {
+                defaultCompanies,
+                defaultProductTypes,
+                defaultConsoles,
+                defaultProducts,
+                defaultUserRoles,
+                defaultUsers,
+                defaultCustomers,
+                defaultTransactions,
+                defaultTransactionLineItems
+            };
+
+            MySqlConnection dbConn = new MySqlConnection(DBConnectionString);
+
+            try {
+                dbConn.Open();
+
+                for(int i = 0; i < defaultCommands.Length; i++) {
+                    MySqlCommand command = new MySqlCommand(defaultCommands[i], dbConn);
+                    int statementCount = command.ExecuteNonQuery();
+
+                    if(statementCount == 0) {
+                        // Statement Did not Run
+
+                    }
+                }
+            }
+            catch(MySqlException ex) {
+                MessageBox.Show(ex.Message);
+            }
+            finally {
+                dbConn.Close();
+            }
+        }
 
         private static void GetAllUserRoles(MySqlConnection dbConn) {
             // Load User Roles
@@ -267,6 +357,219 @@ namespace RetroCollector.Data.Management {
             DBSetupForm newDbSetupForm = new DBSetupForm();
             newDbSetupForm.FormSaved += OnDbSetupSaved;
             newDbSetupForm.ShowDialog();
+        }
+        private static void InitializeSchema() {
+            // Create Schema Command
+            string createSchema = $"CREATE SCHEMA `retrocollector`;";
+
+            // Create Companies Table Command
+            string createCompaniesTable =
+                $"CREATE TABLE `retrocollector`.`companies` (" +
+                $"`companyId` INT NOT NULL," +
+                $"`name` VARCHAR(45) NOT NULL," +
+                $"`dateCreated` DATETIME NULL," +
+                $"`dateLastUpdated` DATETIME NULL," +
+                $"`createdBy` VARCHAR(45) NULL," +
+                $"`lastUpdatedBy` VARCHAR(45) NULL," +
+                $"PRIMARY KEY (`companyId`)," +
+                $"UNIQUE INDEX `companyId_UNIQUE` (`companyId` ASC) VISIBLE);";
+
+            // Create Product Types Table Command
+            string createProductTypesTable =
+                $"CREATE TABLE `retrocollector`.`producttypes` (" +
+                $"`productTypeId` INT NOT NULL," +
+                $"`name` VARCHAR(45) NOT NULL," +
+                $"PRIMARY KEY(`productTypeId`)," +
+                $"UNIQUE INDEX `productTypeId_UNIQUE` (`productTypeId` ASC) VISIBLE," +
+                $"UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE);";
+
+            // Create Products Table Command
+            string createProductsTable =
+                $"CREATE TABLE `retrocollector`.`products` (" +
+                $"`productId` INT NOT NULL," +
+                $"`name` VARCHAR(45) NOT NULL," +
+                $"`consoleId` INT NULL," +
+                $"`developerId` INT NOT NULL," +
+                $"`dateReleased` DATETIME NULL," +
+                $"`quality` INT NULL," +
+                $"`completion` INT NULL," +
+                $"`region` INT NULL," +
+                $"`productTypeId` INT NOT NULL," +
+                $"`cost` DECIMAL NOT NULL," +
+                $"`price` DECIMAL NOT NULL," +
+                $"`dateCreated` DATETIME NULL," +
+                $"`dateLastUpdated` DATETIME NULL," +
+                $"`createdBy` VARCHAR(45) NULL," +
+                $"`lastUpdatedBy` VARCHAR(45) NULL," +
+                $"PRIMARY KEY(`productId`)," +
+                $"UNIQUE INDEX `productId_UNIQUE` (`productId` ASC) VISIBLE," +
+                $"INDEX `productDeveloperId_idx` (`developerId` ASC) VISIBLE," +
+                $"INDEX `productTypeId_idx` (`productTypeId` ASC) VISIBLE," +
+                $"CONSTRAINT `productDeveloperId`" +
+                $"FOREIGN KEY(`developerId`)" +
+                $"REFERENCES `retrocollector`.`companies` (`companyId`)" +
+                $"ON DELETE NO ACTION" +
+                $"ON UPDATE NO ACTION," +
+                $"CONSTRAINT `productTypeId`" +
+                $"FOREIGN KEY(`productTypeId`)" +
+                $"REFERENCES `retrocollector`.`producttypes` (`productTypeId`)" +
+                $"ON DELETE RESTRICT" +
+                $"ON UPDATE NO ACTION);";
+
+            // Create Customers Table Command
+            string createCustomersTable =
+                $"CREATE TABLE `retrocollector`.`customers` (" +
+                $"  `customerId` INT NOT NULL," +
+                $"  `firstName` VARCHAR(45) NULL," +
+                $"  `lastName` VARCHAR(45) NULL," +
+                $"  `address1` VARCHAR(45) NULL," +
+                $"  `address2` VARCHAR(45) NULL," +
+                $"  `city` VARCHAR(45) NULL," +
+                $"  `stateAbbr` VARCHAR(2) NULL," +
+                $"  `zipCode` VARCHAR(5) NULL," +
+                $"  `phoneNumber` VARCHAR(14) NULL," +
+                $"  `email` VARCHAR(45) NOT NULL," +
+                $"  PRIMARY KEY(`customerId`)," +
+                $"  UNIQUE INDEX `customerId_UNIQUE` (`customerId` ASC) VISIBLE);";
+
+            // Create User Roles Table Command
+            string createRolesTable =
+                $"CREATE TABLE `retrocollector`.`userroles` (" +
+                $"  `roleId` INT NOT NULL," +
+                $"  `name` VARCHAR(45) NOT NULL," +
+                $"  `description` VARCHAR(45) NULL," +
+                $"  `allowCreateProducts` TINYINT NOT NULL," +
+                $"  `allowEditProducts` TINYINT NOT NULL," +
+                $"  `allowDeleteProducts` TINYINT NOT NULL," +
+                $"  `allowCreateUsers` TINYINT NOT NULL," +
+                $"  `allowEditUsers` TINYINT NOT NULL," +
+                $"  `allowDeleteUsers` TINYINT NOT NULL," +
+                $"  `allowCreateRoles` TINYINT NOT NULL," +
+                $"  `allowEditRoles` TINYINT NOT NULL," +
+                $"  `allowDeleteRoles` TINYINT NOT NULL," +
+                $"  `allowReporting` TINYINT NOT NULL," +
+                $"  `allowProcessSales` TINYINT NOT NULL," +
+                $"  `allowAdminOptions` TINYINT NOT NULL," +
+                $"  `dateCreated` DATETIME NULL," +
+                $"  `dateLastUpdated` DATETIME NULL," +
+                $"  `createdBy` VARCHAR(45) NULL," +
+                $"  `lastUpdatedBy` VARCHAR(45) NULL," +
+                $"  PRIMARY KEY(`roleId`)," +
+                $"  UNIQUE INDEX `roleId_UNIQUE` (`roleId` ASC) VISIBLE);";
+
+            // Create Users Table Command
+            string createUsersTable =
+                $"CREATE TABLE `retrocollector`.`users` (" +
+                $"  `userId` INT NOT NULL," +
+                $"  `firstName` VARCHAR(45) NOT NULL," +
+                $"  `lastName` VARCHAR(45) NOT NULL," +
+                $"  `userName` VARCHAR(45) NOT NULL," +
+                $"  `passHash` VARCHAR(256) NOT NULL," +
+                $"  `passSalt` VARCHAR(100) NOT NULL," +
+                $"  `dateCreated` DATETIME NULL," +
+                $"  `dateLastUpdated` DATETIME NULL," +
+                $"  `createdBy` VARCHAR(45) NULL," +
+                $"  `lastUpdatedBy` VARCHAR(45) NULL," +
+                $"  `roleId` INT NOT NULL," +
+                $"  PRIMARY KEY(`userId`)," +
+                $"  UNIQUE INDEX `userId_UNIQUE` (`userId` ASC) VISIBLE," +
+                $"  UNIQUE INDEX `userName_UNIQUE` (`userName` ASC) VISIBLE," +
+                $"  CONSTRAINT `userrolekey`" +
+                $"    FOREIGN KEY(`roleId`)" +
+                $"    REFERENCES `retrocollector`.`userroles` (`roleId`)" +
+                $"    ON DELETE NO ACTION" +
+                $"    ON UPDATE NO ACTION);";
+
+            // Create Transactions Table Command
+            string createTransactionsTable =
+                $"CREATE TABLE `retrocollector`.`transactions` (" +
+                $"  `transactionId` INT NOT NULL," +
+                $"  `description` VARCHAR(45) NULL," +
+                $"  `dateOfSale` DATETIME NOT NULL," +
+                $"  `salesRepId` INT NOT NULL," +
+                $"  `customerId` INT NOT NULL," +
+                $"  `dateCreated` DATETIME NULL," +
+                $"  `dateLastUpdated` DATETIME NULL," +
+                $"  `createdBy` VARCHAR(45) NULL," +
+                $"  `lastUpdatedBy` VARCHAR(45) NULL," +
+                $"  PRIMARY KEY(`transactionId`)," +
+                $"  UNIQUE INDEX `transactionId_UNIQUE` (`transactionId` ASC) VISIBLE," +
+                $"  INDEX `transactionSalesRepId_idx` (`salesRepId` ASC) VISIBLE," +
+                $"  INDEX `transactionCustomerId_idx` (`customerId` ASC) VISIBLE," +
+                $"  CONSTRAINT `transactionSalesRepId`" +
+                $"    FOREIGN KEY(`salesRepId`)" +
+                $"    REFERENCES `retrocollector`.`users` (`userId`)" +
+                $"    ON DELETE NO ACTION" +
+                $"    ON UPDATE NO ACTION," +
+                $"  CONSTRAINT `transactionCustomerId`" +
+                $"    FOREIGN KEY(`customerId`)" +
+                $"    REFERENCES `retrocollector`.`customers` (`customerId`)" +
+                $"    ON DELETE NO ACTION" +
+                $"    ON UPDATE NO ACTION);";
+
+            // Create Transaction Line Items Table Command
+            string createTransactionLineItemsTable =
+                $"CREATE TABLE `retrocollector`.`transactionlineitems` (" +
+                $"  `transactionId` INT NOT NULL," +
+                $"  `productId` INT NOT NULL," +
+                $"  `quantity` INT NULL," +
+                $"  `pricePerProduct` DECIMAL NULL," +
+                $"  PRIMARY KEY(`transactionId`, `productId`)," +
+                $"  INDEX `lineItemProductId_idx` (`productId` ASC) VISIBLE," +
+                $"  CONSTRAINT `lineItemTransactionId`" +
+                $"    FOREIGN KEY(`transactionId`)" +
+                $"    REFERENCES `retrocollector`.`transactions` (`transactionId`)" +
+                $"    ON DELETE NO ACTION" +
+                $"    ON UPDATE NO ACTION," +
+                $"  CONSTRAINT `lineItemProductId`" +
+                $"    FOREIGN KEY(`productId`)" +
+                $"    REFERENCES `retrocollector`.`products` (`productId`)" +
+                $"    ON DELETE NO ACTION" +
+                $"    ON UPDATE NO ACTION);";
+
+            // Create Consoles Table Command
+            string createConsolesTable =
+                $"CREATE TABLE `retrocollector`.`consoles` (" +
+                $"  `consoleId` INT NOT NULL," +
+                $"  `name` VARCHAR(45) NOT NULL," +
+                $"  `developerName` VARCHAR(45) NULL," +
+                $"  PRIMARY KEY(`consoleId`)," +
+                $"  UNIQUE INDEX `consoleId_UNIQUE` (`consoleId` ASC) VISIBLE);";
+
+            string[] mysqlCommandStrings = new string[] {
+                createSchema,
+                createCompaniesTable,
+                createProductTypesTable,
+                createProductsTable,
+                createCustomersTable,
+                createRolesTable,
+                createUsersTable,
+                createTransactionsTable,
+                createTransactionLineItemsTable,
+                createConsolesTable };
+
+            MySqlConnection dbConn = new MySqlConnection(DBConnectionString);
+
+            try {
+                dbConn.Open();
+
+                for(int i = 0; i < mysqlCommandStrings.Length; i++) {
+                    MySqlCommand command = new MySqlCommand(mysqlCommandStrings[i], dbConn);
+                    int statementCount = command.ExecuteNonQuery();
+
+                    if(statementCount == 0) {
+                        // Statement Did Not Run
+                        MessageBox.Show($"Issue running command. No statements were processed.\r\n\r\n Statement In Question:\r\n\r\n{mysqlCommandStrings[i]}");
+                    }
+                }
+
+            }
+            catch(MySqlException ex) {
+                MessageBox.Show(ex.Message);
+            }
+            finally {
+                dbConn.Close();
+            }
         }
 
         public static UserAccount GetUserByUsername(string username) {
